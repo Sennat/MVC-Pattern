bb9a78ac5c0ea7a0879df1616b0bc3b1b927c478
Natnael Alemayehu <dinksira@gmail.com>
2017-11-24 22:48:45 -0600

    Update Model.java

--- src/mvc/Model.java
+++ src/mvc/Model.java
@@ -1,9 +1,3 @@
-/* ************************************************************************************
- * Copyright (C) Nov 24, 2017, Natnael Alemayehu, All rights Reserved. Unauthorized 
- * copying of this file and/or distributed without, the express permission of 
- * Natnael Alemayehu is strictly prohibited. Written by Natnael Alemayehu 2:05:38 AM.
- * ************************************************************************************
- */
 
 package mvc;
 
