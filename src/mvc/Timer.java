4e2fe5bb3b797b31ff509d1c5e76425541ed30aa
Natnael Alemayehu <dinksira@gmail.com>
2017-11-24 22:51:38 -0600

    Update Timer.java

--- src/mvc/Timer.java
+++ src/mvc/Timer.java
@@ -1,9 +1,3 @@
-/* ************************************************************************************
- * Copyright (C) Nov 24, 2017, Natnael Alemayehu, All rights Reserved. Unauthorized 
- * copying of this file and/or distributed without, the express permission of 
- * Natnael Alemayehu is strictly prohibited. Written by Natnael Alemayehu 6:49:35 AM.
- * ************************************************************************************
- */
 
 package mvc;
 
