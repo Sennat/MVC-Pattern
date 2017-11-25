5282bca92cf01163e754f334883035e9d75b6c0a
Natnael Alemayehu <dinksira@gmail.com>
2017-11-24 22:50:49 -0600

    Update Payment.java

--- src/mvc/Payment.java
+++ src/mvc/Payment.java
@@ -1,9 +1,3 @@
-/* ************************************************************************************
- * Copyright (C) Nov 24, 2017, Natnael Alemayehu, All rights Reserved. Unauthorized 
- * copying of this file and/or distributed without, the express permission of 
- * Natnael Alemayehu is strictly prohibited. Written by Natnael Alemayehu 2:10:48 AM.
- * ************************************************************************************
- */
 
 package mvc;
 
@@ -14,6 +8,9 @@ package mvc;
  */
 public interface Payment {
 	
+	/**
+ 	* A method to calculate a fee
+ 	*/
 	public int calculateFees();
 
 }
